9a29b0f617d32a83740c2287d2a2fa65b603e1e8
jozli <joseline.alfaro033@gmail.com>
2017-06-15 01:50:03 -0600

    Create RolesControllerMySQL.java

---
+++ src/java/controllers/RolesControllerMySQL.java
@@ -0,0 +1,127 @@
+package controllers;
+
+import com.opensymphony.xwork2.ActionSupport;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import models.RolConectar;
+import models.RolModel;
+
+public class RolesControllerMySQL extends ActionSupport{
+    
+    private RolConectar con;
+    private ArrayList<RolModel> datos;
+    private ArrayList<String> select;
+    private ResultSet dato;
+    
+    private int rol_id;
+    private String rol_descripcion;
+
+    public RolConectar getCon() {
+        return con;
+    }
+
+    public void setCon(RolConectar con) {
+        this.con = con;
+    }
+
+    public ArrayList<RolModel> getDatos() {
+        return datos;
+    }
+
+    public void setDatos(ArrayList<RolModel> datos) {
+        this.datos = datos;
+    }
+
+    
+    public ArrayList<String> getSelect() {
+        return select;
+    }
+
+    public void setSelect(ArrayList<String> select) {
+        this.select = select;
+    }
+
+    public ResultSet getDato() {
+        return dato;
+    }
+
+    public void setDato(ResultSet dato) {
+        this.dato = dato;
+    }
+
+    public int getRol_id() {
+        return rol_id;
+    }
+
+    public void setRol_id(int rol_id) {
+        this.rol_id = rol_id;
+    }
+
+    
+
+    public String getRol_descripcion() {
+        return rol_descripcion;
+    }
+
+    public void setRol_descripcion(String rol_descripcion) {
+        this.rol_descripcion = rol_descripcion;
+    }
+
+    
+ 
+    @Override
+    public String execute() throws Exception {     
+        this.con = new RolConectar();
+        this.datos = new ArrayList<>();
+        this.datos = con.getData("select * from rol_rol");
+        
+        return SUCCESS;
+    }
+    
+    public String recibirDatos() throws Exception {
+        this.con = new RolConectar();      
+              
+        if (this.rol_id == 0)       
+            con.setData("CALL `sp_insert_rol_rol`('"+this.rol_descripcion+"')");
+        else 
+            con.updateData("update rol_rol set rol_descripcion="+this.rol_descripcion+" where rol_id="+this.rol_id+"");
+
+        this.rol_id = 0;
+        this.rol_descripcion = null;
+        
+
+        this.datos = new ArrayList<>();
+        this.datos = con.getData("select * from rol_rol");
+
+        return SUCCESS;
+    }
+
+    public String eliminar() throws Exception {
+        this.con = new RolConectar();      
+        
+        con.deleteData("delete from rol_rol where rol_id="+this.rol_id+"");
+        this.datos = new ArrayList<>();
+        this.datos = con.getData("select * from rol_rol");
+        
+        return SUCCESS;        
+    }
+
+    public String llenarFormulario() throws Exception{
+        this.con = new RolConectar();      
+           
+        
+        this.dato = con.getDataForm("select * from rol_rol where rol_id="+this.rol_id+"");
+        while(this.dato.next()){
+            
+        this.rol_id = dato.getInt("rol_id");
+        this.rol_descripcion = dato.getString("rol_descripcion");
+        ;    
+
+        }
+        this.datos = new ArrayList<>();
+        this.datos = con.getData("select * from rol_rol");
+        
+        return SUCCESS;       
+    }    
+
+}
